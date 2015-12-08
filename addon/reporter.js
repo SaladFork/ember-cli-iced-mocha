e27c53f0736fe0609ee3fe7b04ee7418464de957
Elad Shahar <SaladFork@Gmail.com>
2015-12-08 10:19:05 -0500

    Add support for tracking pass and failure events from the runner

--- addon/reporter.js
+++ addon/reporter.js
@@ -1,6 +1,23 @@
 export default class Reporter {
-  constructor() {
+  constructor(runner) {
     this.passes = 0;
     this.failures = 0;
+
+    this.runner = runner;
+    this.addEventListeners(runner);
+  }
+
+  addEventListeners(runner) {
+    // TODO: Generalize this to avoid duplication
+    runner.on('pass', this.onPass.bind(this));
+    runner.on('fail', this.onFail.bind(this));
+  }
+
+  onPass() {
+    this.passes++;
+  }
+
+  onFail() {
+    this.failures++;
   }
 }
--- tests/unit/reporter-test.js
+++ tests/unit/reporter-test.js
@@ -1,23 +1,51 @@
 import {
   beforeEach,
   describe,
+  context,
   it,
 } from 'mocha';
 
 import { expect } from 'chai';
 
+import MockRunner from '../helpers/mock-runner';
+
 import Reporter from 'ember-cli-iced-mocha/reporter';
 
 describe('Reporter', function() {
 
-  let reporter;
+  let reporter, runner;
   beforeEach(function() {
-    reporter = new Reporter();
+    runner = new MockRunner();
+    reporter = new Reporter(runner);
   });
 
   it('should start with 0 passing tests and 0 failing tests', function() {
     expect(reporter.passes).to.equal(0);
     expect(reporter.failures).to.equal(0);
+  });
+
+  context('when a test passes', function() {
+
+    beforeEach(function() {
+      runner.trigger('pass');
+    });
+
+    it('should increment the passed tests count', function() {
+      expect(reporter.passes).to.equal(1);
+    });
+
+  });
+
+  context('when a test fails', function() {
+
+    beforeEach(function() {
+      runner.trigger('fail');
+    });
+
+    it('should increment the failed tests count', function() {
+      expect(reporter.failures).to.equal(1);
+    });
+
   });
 
 });
